d986f3acd737e7527d44b183027dd87e60a02361
bryan-bmf <111380447+bryan-bmf@users.noreply.github.com>
2024-09-25 19:38:29 -0400

    Header (#1)
    * initial header
    
    * changed menu to drawer
    
    * unformatted box score with api data
    
    * finished score box
    
    * display logic for live games
    
    * show game summary when finished
    
    * add switch button

---
+++ src/components/BoxScore.tsx
@@ -0,0 +1,185 @@
+import {
+    Box,
+    Divider,
+    List,
+    ListItem,
+    ListItemText,
+    Typography,
+} from "@mui/material";
+import { AnyObject } from "../types";
+
+const BoxScore = ({ game }: Props) => {
+    // calculate team's series record
+	const calculateSeriesRecord = (teamId: string) => {
+		let seriesRecord = "0-0";
+		let wins = 0;
+		let losses = 0;
+
+		game.competitions[0].series.competitors.forEach((team: AnyObject) => {
+			// count wins if correct team
+			if (team.id === teamId) {
+				wins = team.wins;
+			}
+			// count other team's wins as losses for current team
+			else {
+				losses = team.wins;
+			}
+		});
+
+		seriesRecord = `${wins}-${losses}`;
+		return seriesRecord;
+	};
+
+	// find home/away teams index
+	const homeIndex = game.competitions[0].competitors.findIndex(
+		(team: AnyObject) => team.homeAway === "home"
+	);
+	const awayIndex = game.competitions[0].competitors.findIndex(
+		(team: AnyObject) => team.homeAway === "away"
+	);
+
+    // teams
+	const home = game.competitions[0].competitors[homeIndex];
+	const away = game.competitions[0].competitors[awayIndex];
+
+    // if game has not started, it will show date and time of game
+    // else quarter and time left
+	let gameDetails = game.status.type.shortDetail;
+    let homeScore;
+    let awayScore;
+    let gameStatus = parseInt(game.status.type.id);
+    let fontColor = null;
+
+    if(gameStatus === 1)
+        gameDetails = gameDetails.replace(" EDT", "");
+    else {
+        homeScore = home.score;
+        awayScore = away.score;
+        fontColor = sx.gameDetails;
+    }
+
+	// home stuff
+	const homeId = home.id;
+	const homeTeam = home.team.abbreviation;
+	const homeTeamRecord = home.records[0].summary;
+	const homeLogo = home.team.logo;
+
+	// away stuff
+	const awayId = away.id;
+	const awayTeam = away.team.abbreviation;
+	const awayTeamRecord = away.records[0].summary;
+	const awayLogo = away.team.logo;
+
+	// preseason, regular season or playoffs
+	const seasonType = game.competitions[0].series.type;
+    const seriesSummary = <Typography variant="caption" sx={sx.summary}>{game.competitions[0].series.summary}</Typography>;
+
+    // calculate playoff series record for teams in playoffs 
+    let homePlayoffSeriesRecord;
+    let awayPlayoffSeriesRecord;
+	if (seasonType === "playoff") {
+		homePlayoffSeriesRecord = calculateSeriesRecord(homeId);
+		awayPlayoffSeriesRecord = calculateSeriesRecord(awayId);
+	}
+
+	const teams = [
+		{
+			id: awayId,
+			team: awayTeam,
+			logo: awayLogo,
+			record: seasonType === "playoff" ? awayPlayoffSeriesRecord : awayTeamRecord,
+            score: awayScore
+		},
+		{
+			id: homeId,
+			team: homeTeam,
+			logo: homeLogo,
+			record: seasonType === "playoff" ? homePlayoffSeriesRecord : homeTeamRecord,
+            score: homeScore
+		},
+	];
+
+	return (
+		<Box sx={sx.out}>
+			<Box sx={sx.box}>
+                {/* GAME DETAILS */}
+				<Box>
+					<Typography variant="caption" sx={fontColor}>{gameDetails}</Typography>
+                    {/* Show summary if game has ended */}
+                    {gameStatus === 3 ? seriesSummary : null}
+				</Box>
+				<List sx={sx.list}>
+					{teams.map((team) => (
+						<ListItem key={team.id} sx={sx.item}>
+                            {/* TEAM LOGO AND NAME */}
+							<Box sx={sx.team}>
+								<img
+									src={team.logo}
+									alt={team.team}
+									width="22"
+									height="22"
+								/>
+								<ListItemText
+									primary={team.team}
+									primaryTypographyProps={{ variant: "caption" }}
+									sx={sx.logo}
+								/>
+							</Box>
+                            {/* TEAM RECORD OR GAME SCORE */}
+							<Box>
+                                {/* If the game is in progress, show game score instead of team record */}
+								<ListItemText
+									secondary={gameStatus > 1 ? team.score : team.record}
+									secondaryTypographyProps={{ variant: "caption" }}
+								/>
+							</Box>
+						</ListItem>
+					))}
+				</List>
+			</Box>
+			<Divider orientation="vertical" variant="middle" flexItem />
+		</Box>
+	);
+};
+
+const sx = {
+    out: {
+		display: "flex",
+	},
+	box: {
+		width: "auto",
+        maxWidth: "200px",
+        minWidth: "100px",
+		height: "75px",
+		padding: 1,
+	},
+	item: {
+		p: 0,
+		display: "flex",
+		justifyContent: "space-between",
+	},
+	team: {
+		display: "flex",
+		alignItems: "center",
+		justifyContent: "space-between",
+	},
+	logo: {
+		marginLeft: "5px",
+	},
+	list: {
+		p: 0,
+	},
+    gameDetails: {
+        color: "red",
+        fontWeight: "bold",
+    },
+    summary: {
+        marginLeft: 2,
+    },
+};
+
+interface Props {
+	game: AnyObject;
+}
+
+export default BoxScore;
---
+++ src/components/Scoreboard.tsx
@@ -0,0 +1,48 @@
+import { Box } from "@mui/material";
+import { useEffect, useState } from "react";
+import { AnyObject } from "../types";
+import BoxScore from "./BoxScore";
+
+const Scoreboard = () => {
+	const [games, setGames] = useState<Array<AnyObject>>();
+
+	const fetchGames = async (controller: AbortController) => {
+		try {
+			const url =
+				"http://site.api.espn.com/apis/site/v2/sports/basketball/wnba/scoreboard";
+			const resp = await fetch(url, {
+				// attach the controller to the request
+				signal: controller.signal,
+			});
+			const data = await resp.json();
+			setGames(data.events);
+		} catch (error) {
+            console.error("Error fetching games:", error);
+            setGames([]);
+        }
+	};
+
+	useEffect(() => {
+		//create the abort controller
+		let controller = new AbortController();
+
+		fetchGames(controller);
+		// Cleanup function
+		return () => controller?.abort();
+	}, []);
+
+	return (
+		<Box sx={sx.scoreboard}>
+			{games && games.map((game: AnyObject) => <BoxScore key={game.id} game={game} />)}
+		</Box>
+	);
+};
+
+const sx = {
+	scoreboard: {
+		display: "flex",
+		overflow: "auto",
+	},
+};
+
+export default Scoreboard;
